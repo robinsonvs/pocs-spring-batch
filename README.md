b1f247f5d47d2e9899687c04d79c0786bc999e5a
robinsonvs <robinsonvs@gmail.com>
2020-09-04 00:10:29 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# pocs-spring-batch
---
+++ firstspringbatchapplication/src/test/java/com/severo/firstspringbatchapplication/FirstspringbatchapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.severo.firstspringbatchapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstspringbatchapplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
