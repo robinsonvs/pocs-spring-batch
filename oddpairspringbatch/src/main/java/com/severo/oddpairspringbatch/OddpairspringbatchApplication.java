2d3807103917a7af4169799a90023c9a38d4ae53
robinsonvs <robinsonvs@gmail.com>
2020-09-04 21:24:50 -0300

    create step chunk based

---
+++ oddpairspringbatch/src/main/java/com/severo/oddpairspringbatch/OddpairspringbatchApplication.java
@@ -0,0 +1,13 @@
+package com.severo.oddpairspringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OddpairspringbatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OddpairspringbatchApplication.class, args);
+    }
+
+}
---
+++ oddpairspringbatch/src/test/java/com/severo/oddpairspringbatch/OddpairspringbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.severo.oddpairspringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OddpairspringbatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
